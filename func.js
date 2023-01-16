782561e2932872bf5642f25876b7514b4f8e99fe
Abhishek@26 <ajaiswal@deqode.com>
2023-01-16 19:54:06 +0530

    check function added

--- func.js
+++ func.js
@@ -46,6 +46,25 @@ function validate() {
     }    
     else{
         alert('Registered successfully')
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
         window.location.href = '/login.html'
     }  
 }    
+
+
+//checking
+function check(){
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('You are logged in.');
+    }else{
+        alert('Error on login');
+    }
+}
