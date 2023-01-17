0ca34841eff60a8228c8684931bb590887952d5c
Abhishek@26 <ajaiswal@deqode.com>
2023-01-17 14:28:07 +0530

    added done

--- func.js
+++ func.js
@@ -1,70 +1,119 @@
 
 
 function validate() {    
-    var fname = document.reg_form.fname;    
-    var lname = document.reg_form.lname;    
-    var username = document.reg_form.username;  
-    var passw = document.reg_form.password;  
-    var gender = document.reg_form.gender;    
-    var email = document.reg_form.email;    
-    var role = document.reg_form.role;    
+    var fname = document.reg_form.fname.value;    
+    var lname = document.reg_form.lname.value;    
+    var username = document.reg_form.username.value;  
+    var passw = document.reg_form.password.value;  
+    var gender = document.reg_form.gender.value;    
+    var email = document.reg_form.email.value;    
+    var role = document.reg_form.role.value;    
 
-    if (fname.value.length <= 0) {    
+    if (fname.length <= 0) {    
         alert("Name is required");    
         fname.focus();    
         return false;    
     }    
-    else if (lname.value.length <= 0) {    
+    else if (lname.length <= 0) {    
         alert("Last Name is required");    
         lname.focus();    
         return false;    
     }    
-    else if (passw.value.length <=7){
+    else if (passw.length <=7){
         alert("Password is too Small")
         passw.focus();
         return false;
     }
-    else if (username.value.length <= 0) {    
+    else if (username.length <= 0) {    
         alert("Address is required");    
         address.focus();    
         return false;    
     }    
-    else if (gender.value.length <= 0) {    
+    else if (gender.length <= 0) {    
         alert("Gender is required");    
         gender.focus();    
         return false;    
     }    
-    else if (email.value.length <= 0) {    
+    else if (email.length <= 0) {    
         alert("Email Id is required");    
         email.focus();    
         return false;    
     }       
-    else if (role.value == "select Role") {    
+    else if (role== "select Role") {    
         alert("Role is required");    
         course.focus();    
         return false;    
     }    
     else{
-        alert('Registered successfully')
-        localStorage.setItem('name', name.value);
-        localStorage.setItem('pw', pw.value);
-        window.location.href = '/login.html'
-    }  
-}    
 
+        var userdetail = {
+            'email': email,
+            'password': passw,
+            'firstname':fname,
+            'lastname':lname,
+            'gender':gender,
+            'role':role
+
+        };
+        
+        localStorage.setItem(username, JSON.stringify(userdetail));
+        
+        
+
+    alert('Registered successfully')
+    window.location.href = '/login.html'
+}    
+}
+
+function listusers(role){
+    // populate the user details 
+    str = ""
+    let tableBody = document.getElementById("tableBody");
+
+    for (x in localStorage){
+        let username = x;
+        let userFromLocalStorage = JSON.parse(localStorage.getItem(username));
+        if (userFromLocalStorage.role == role){
+            str += `
+        <tr>
+        <td>${userFromLocalStorage.firstname}</td>
+        <td>${userFromLocalStorage.lastname}</td> 
+        <td>${username}</td>
+        <td>${userFromLocalStorage.email}</td>
+        <td>${userFromLocalStorage.role}</td>
+        <td>${userFromLocalStorage.gender}</td>
+        </tr>`; 
+        tableBody.innerHTML = str;
+        }
+        
+
+    }
+}
 
 //checking
 function check(){
-    var storedName = localStorage.getItem('name');
-    var storedPw = localStorage.getItem('pw');
-
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-    var userRemember = document.getElementById("rememberMe");
-
-    if(userName.value == storedName && userPw.value == storedPw){
-        alert('You are logged in.');
-    }else{
-        alert('Error on login');
+    console.warn('called')
+    var username = document.getElementById("username").value; // username of user you want to log in as
+    var userFromLocalStorage = JSON.parse(localStorage.getItem(username));
+    if(!userFromLocalStorage){
+        alert('user doesnt exist');
+        return;
     }
-}
+    
+    var password = document.getElementById("password").value;
+    
+    if (userFromLocalStorage.password !== password){
+        alert('wrong password');
+        return;
+    }
+    
+    alert(`logged in as ${userFromLocalStorage.role} successfully`);
+    listusers(userFromLocalStorage.role)
+    
+
+    
+    
+};
+
+login_button  = document.getElementById('login-button')
+login_button.addEventListener('click',check)
